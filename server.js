882d1b9844f49e2a475c36a6b636c5b141176593
admin <ashishmerani@gmail.com>
2016-04-18 09:47:06 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,115 @@
+// Client-side code                                                             
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, l    atedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trai    ling: true */
+// Server-side code                                                             
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, new    cap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+"use strict";
+
+
+var bodyParser = require("body-parser");
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+
+// Mongoose Schema definition                                                   
+var Schema = mongoose.Schema;
+var LinkSchema = new Schema({
+    title: String,
+    link: String,
+    clicks: {
+        type: Number,
+        default: 0
+    },
+    __v: {
+        type: Number,
+        select: false
+    }
+});
+// Mongoose Model definition                                                    
+var Link = mongoose.model("Link", LinkSchema);
+
+
+
+app.use(bodyParser.json());
+
+
+
+app.get("/links", function(req, res) {
+    Link.find({}, "-_id", function(err, data) {
+        if (err !== null) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.get("/click/:title", function(req, res) {
+    var link;
+    Link.find({
+        title: req.params.title
+    }, function(err, body) {
+        if (err !== null) {
+            res.send(err);
+        } else if (body === null) {
+            res.send("value not found");
+        } else {
+            link = body.link;
+            console.log(body);
+            console.log("LINk is: " + link);
+            // res.send("click is set to " + req.params.title);                          
+            Link.update({
+                title: req.params.title,
+                link
+            }, {
+                $inc: {
+                    clicks: 4
+                }
+            }, function(err, data) {
+                console.log(data);
+                if (err !== null) {
+                    res.send(err);
+                }
+            });
+            res.redirect(link);
+        }
+    });
+});
+app.post("/links", function(req, res) {
+
+    console.log(req.body);
+    var newLink = new Link({
+        "title": req.body.title,
+        "link": req.body.link
+    });
+    newLink.save(function(error, result) {
+        console.log(result);
+        if (error !== null) {
+            console.log(error);
+            res.send(error);
+        } else {
+            Link.find({}, function(err, result) {
+                if (err !== null) {
+                    res.send(error);
+                }
+
+                res.json(result);
+            });
+        }
+    });
+});
+
+var port = 3000;
+app.listen(port, function() {
+    console.log("listening on http://localhost:", port);
+
+    var mongoURI = "mongodb://localhost/mydb";
+    var MongoDB = mongoose.connect(mongoURI).connection;
+    MongoDB.on("error", function(err) {
+        console.log(err.message);
+    });
+    MongoDB.once("open", function() {
+        console.log("mongodb connection open");
+
+
+    });
+});
